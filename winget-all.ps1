955d8e3e5054a37b201ed6cbb80425d22a48d8bf
Felix Scholze <felix@webshaped.de>
2022-09-02 16:55:03 +0200

    Update winget-all.ps1

--- winget-all.ps1
+++ winget-all.ps1
@@ -1,42 +1,50 @@
 Echo Start installing all programs via winget
 
-winget install -e --id Microsoft.VisualStudioCode;
 winget install -e --id Discord.Discord;
-winget install -e --id Docker.DockerDesktop;
-winget install -e --id Valve.Steam;
-winget install -e --id Notepad++.Notepad++;
 winget install -e --id OpenWhisperSystems.Signal;
-winget install -e --id BraveSoftware.BraveBrowser;
-winget install -e --id Canonical.Ubuntu;
-winget install -e --id lukehaas.RunJS;
 winget install -e --id dev47apps.DroidCam;
 winget install -e --id Figma.Figma;
-winget install -e --id Fork.Fork;
 winget install -e --id Foxit.FoxitReader;
-winget install -e --id Git.Git;
-winget install -e --id Google.Chrome;
-winget install -e --id Microsoft.WindowsTerminal;
 winget install -e --id Mozilla.Thunderbird;
-winget install -e --id Opera.Opera;
-winget install -e --id Postman.Postman;
-winget install -e --id ScreamingFrog.SEOSpider;
-winget install -e --id TablePlus.TablePlus;
 winget install -e --id Telegram.TelegramDesktop;
 winget install -e --id KDE.Kdenlive;
-winget install -e --id GitHub.cli;
-winget install -e --id Microsoft.PowerShell;
-winget install -e --id EpicGames.EpicGamesLauncher;
 winget install -e --id Microsoft.PowerToys;
 winget install -e --id KeePassXCTeam.KeePassXC;
 winget install -e --id Nextcloud.NextcloudDesktop;
-winget install -e --id Nvidia.GeForceExperience;
 winget install -e --id RealVNC.VNCViewer;
-winget install -e --id PuTTY.PuTTY;
 winget install -e --id Philips.HueSync;
-winget install -e --id Mozilla.Firefox;
-winget install -e --id Ubisoft.Connect;
 winget install -e --id DeepL.DeepL;
 winget install -e --id Spotify.Spotify;
+winget install -e --id=IDRIX.VeraCrypt;
+winget install -e --id=VideoLAN.VLC
+
+Echo Install browsers
+winget install -e --id Mozilla.Firefox;
+winget install -e --id Opera.Opera;
+winget install -e --id Google.Chrome;
+winget install -e --id BraveSoftware.BraveBrowser;
+
+Echo Install gaming software
+winget install -e --id Ubisoft.Connect;
+winget install -e --id EpicGames.EpicGamesLauncher;
+winget install -e --id Nvidia.GeForceExperience;
+winget install -e --id Valve.Steam;
+
+Echo Install Dev programs
 winget install -e --id HeidiSQL.HeidiSQL;
+winget install -e --id PuTTY.PuTTY;
+winget install -e --id GitHub.cli;
+winget install -e --id Microsoft.PowerShell;
+winget install -e --id TablePlus.TablePlus;
+winget install -e --id Postman.Postman;
+winget install -e --id ScreamingFrog.SEOSpider;
+winget install -e --id Git.Git;
+winget install -e --id Microsoft.WindowsTerminal;
+winget install -e --id Fork.Fork;
+winget install -e --id Canonical.Ubuntu;
+winget install -e --id lukehaas.RunJS;
+winget install -e --id Docker.DockerDesktop;
+winget install -e --id Notepad++.Notepad++;
+winget install -e --id Microsoft.VisualStudioCode;
 
 Echo all programs installed
