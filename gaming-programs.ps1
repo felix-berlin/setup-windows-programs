bbf27f5f7295813a28a9e57835330629b27e899e
Felix Scholze <felix@webshaped.de>
2022-11-18 12:15:52 +0100

    Create gaming-programs.ps1

---
+++ gaming-programs.ps1
@@ -0,0 +1,7 @@
+Echo Install gaming software
+winget install -e --id Ubisoft.Connect;
+winget install -e --id EpicGames.EpicGamesLauncher;
+winget install -e --id ElectronicArts.EADesktop;
+winget install -e --id Nvidia.GeForceExperience;
+winget install -e --id Valve.Steam;
+winget install -e --id Logitech.GHUB;
