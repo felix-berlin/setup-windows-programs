61dfe84c9cc13d8da6864b88cbd2d61ed4813391
Felix Scholze <felix@webshaped.de>
2022-11-18 12:15:05 +0100

    Create dev-programms.ps1

---
+++ dev-programms.ps1
@@ -0,0 +1,16 @@
+Echo Install dev programs
+winget install -e --id HeidiSQL.HeidiSQL;
+winget install -e --id PuTTY.PuTTY;
+winget install -e --id GitHub.cli;
+winget install -e --id Microsoft.PowerShell;
+winget install -e --id TablePlus.TablePlus;
+winget install -e --id Postman.Postman;
+winget install -e --id ScreamingFrog.SEOSpider;
+winget install -e --id Git.Git;
+winget install -e --id Microsoft.WindowsTerminal;
+winget install -e --id Fork.Fork;
+winget install -e --id Canonical.Ubuntu;
+winget install -e --id lukehaas.RunJS;
+winget install -e --id Docker.DockerDesktop;
+winget install -e --id Notepad++.Notepad++;
+winget install -e --id Microsoft.VisualStudioCode;
